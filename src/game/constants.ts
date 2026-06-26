d418c82dd65b0d70bb66f5413b4c848fa7093279
Kotaro Yamashita <kyamasita@oec.co.jp>
2026-06-26 16:06:28 +0900

    Change enemys formation

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -4,7 +4,7 @@ export const BOARD_COLUMNS = 9
 export const BOARD_ROWS = 9
 export const PLAYER_ROW = BOARD_ROWS - 1
 export const PLAYER_START_COLUMN = Math.floor(BOARD_COLUMNS / 2)
-export const INITIAL_LIVES = 2
+export const INITIAL_LIVES = 4
 export const MAX_BULLETS = 2
 export const SCORE_PER_ENEMY = 100
 export const GAME_TICK_MS = 450
@@ -12,18 +12,18 @@ export const GAME_TICK_MS = 450
 const FORMATIONS: ReadonlyArray<ReadonlyArray<readonly [number, number]>> = [
   [
     [1, 1],
+    [1, 2],
     [1, 3],
-    [1, 5],
-    [1, 7],
+    [1, 4],
     [2, 2],
     [2, 4],
     [2, 6],
   ],
   [
     [1, 2],
+    [1, 3],
     [1, 4],
-    [1, 6],
-    [2, 1],
+    [1, 5],
     [2, 3],
     [2, 5],
     [2, 7],
